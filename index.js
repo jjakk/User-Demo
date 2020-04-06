41736764a2dfb9358f74d938ad45b684cf529fac
Jak <jaksawesome10@gmail.com>
2020-04-06 12:59:45 -0400

    basic stuff

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+  console.log('Running on port ' + port);
+});
+
+app.get('/', (req, res) => {
+  res.send();
+});
