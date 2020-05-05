8a37ce04d7c3509bab3eddef64c0f6c9b63dc174
Jak <jaksawesome10@gmail.com>
2020-05-05 12:05:26 -0400

    not logged in redirect

--- routers/verifyToken.js
+++ routers/verifyToken.js
@@ -7,13 +7,14 @@ app.use(cookieParser());
 module.exports = function(req, res, next){
   //const token = req.header('auth-token');
   const token = req.cookies['auth-token'];
-  if(!token) return res.status(401).send('Access Denied');
+  if(!token) return res.redirect('/login');//res.status(401).send('Access Denied');
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
   }
   catch (e) {
-    res.status(400).send('Invalid Token');
+    //res.status(400).send('Invalid Token');
+    res.redirect('/login');
   }
 }
