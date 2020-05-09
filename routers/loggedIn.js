d1795063ee099dd1ca477ba46b94f2fb00acf745
Jak <jaksawesome10@gmail.com>
2020-05-08 20:05:22 -0400

    name now works

--- routers/loggedIn.js
+++ routers/loggedIn.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const User = require('./../model/User.js');
 const verify = require('./verifyToken');
 
-router.get('/', verify, (req, res) => {
-  res.render('loggedIn');
+router.get('/', verify, async (req, res) => {
+  const user = await User.findOne({_id: req.user._id});
+  if(!user) return res.status(400).send('ID not found');
+  res.render('loggedIn', {
+    name: user.name
+  });
 })
 
 module.exports = router;
